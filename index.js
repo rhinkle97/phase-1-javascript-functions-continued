0330d836653739a9138b6b021010d7ae6e133e0f
Ryan Hinkle <hinkle.ryan97@gmail.com>
2022-02-01 20:33:31 -0800

    commit tree ccd2093b9fa0102b1fc7e31c2d3de0c564c3b92e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun() {
+    return 'This Saturday, I want to roller-skate!';
+}
+
+function mondayWork() {
+    return 'This Monday, I will go to the office.';
+}
+
+function wrapAdjective() {
+    return function innerFunction(special) {
+        let result = wrapAdjective('*')
+        let emphatic = "You are *a hard worker*!"
+        return function innerInnerFunction() {
+            result = wrapAdjective("||")
+            emphatic = "You are ||a dedicated programmer||!"
+        }
+    }
+}
+wrapAdjective('*');
